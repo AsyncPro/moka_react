77fa7b0f181de7afdd64be772a914957e776b72e
AsyncPro <async_pro@outlook.com>
2025-07-20 11:49:07 -0600

    segundo commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const env = loadEnv(mode, process.cwd());
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+      host: env.HOST || '0.0.0.0',
+      port: Number(env.PORT) || 5173,
+    }
+
 })
